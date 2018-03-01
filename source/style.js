2fc03dda0f1b81dd84b508c4b02f6c05ded7ed42
David Skaggs <davidtskaggs@gmail.com>
2018-03-01 10:54:27 -0500

    Add files via upload

---
+++ source/style.js
@@ -0,0 +1,10 @@
+// Shorthand for document ready
+$(function(){
+  $( "#style_editor" ).submit(function( event ) {
+    var selector = $( "input[name*='selector']" ).val()
+    var property = $( "input[name*='property']" ).val()
+    var value = $( "input[name*='value']" ).val()
+    $(selector).css(property, value)
+    event.preventDefault();
+  });
+});
